1ffe10f63f75313c75b50147f31adbe4c0b4780e
Jamal <jamalvga2002@gmail.com>
2024-03-23 00:21:13 +0530

    Add new files and components

--- Front-end/app/layout.tsx
+++ Front-end/app/layout.tsx
@@ -1,8 +1,55 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
+import {
+  Roboto,
+  Sora,
+  Poppins,
+  Rubik,
+  Inter,
+  Urbanist,
+} from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const robotoFont = Roboto({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
+
+const urbanistFont = Urbanist({
+  weight: ["300", "400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-urbanist",
+});
+
+const soraFont = Sora({
+  weight: ["500", "700"],
+  display: "swap",
+  variable: "--font-sora",
+  subsets: ["latin"],
+});
+
+const poppinsFont = Poppins({
+  weight: ["200", "400", "500", "600", "800"],
+  display: "swap",
+  variable: "--font-poppins",
+  subsets: ["latin"],
+});
+
+const rubikFont = Rubik({
+  weight: ["400", "500", "700"],
+  display: "swap",
+  variable: "--font-rubik",
+  subsets: ["latin"],
+});
+
+const interFont = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,9 +61,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const fontVariables = `${interFont.variable} ${robotoFont.variable} ${soraFont.variable} ${poppinsFont.variable} ${rubikFont.variable} ${urbanistFont.variable}`;
+
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={fontVariables}>
+      <body>
+        <Providers>{children}</Providers>{" "}
+      </body>
     </html>
   );
 }
